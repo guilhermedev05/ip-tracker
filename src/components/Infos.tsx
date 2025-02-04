adc582922fab28fce2fbf364646c832cf50e432d
Guilherme Santana <163065225+guilhermedev05@users.noreply.github.com>
2025-02-04 13:23:22 -0300

    style: alterando padding no input e no h1

--- src/components/Infos.tsx
+++ src/components/Infos.tsx
@@ -4,7 +4,7 @@ import { LocationInfo } from "./LocationInfo"
 export const Infos = () => {
     return(
         <div className={`bg-[url(/assets/images/pattern-bg-mobile.png)] bg-center bg-cover bg-no-repeat md:bg-[url(/assets/images/pattern-bg-desktop.png)] h-[40vh] md:h-[35vh] flex flex-col items-center px-5 relative`}>
-          <h1 className="text-lg md:text-2xl py-2 md:py-3 text-center font-['Rubik'] font-[500]">IP Address Tracker</h1>
+          <h1 className="text-lg md:text-2xl py-1.5 md:py-3 text-center font-['Rubik'] font-[500]">IP Address Tracker</h1>
           <InputIp />
           <LocationInfo />
         </div>
